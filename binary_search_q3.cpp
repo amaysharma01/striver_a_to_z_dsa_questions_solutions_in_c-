6d92abfe3a04aa7897286e962722c8f85dca4449
amaysharma01 <amaysharma37@gmail.com>
2026-09-10 19:54:06 +0530

    Implement Upper Bound

---
+++ binary_search_q3.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int func(vector<int> &nums, int k)
+{
+    int n=nums.size();
+    int left=0;
+    int right=n-1;
+    int index=-1;
+    while (left<=right)
+    {
+        int mid=left+(right-left)/2;
+        if (nums[mid]>k)
+        {
+            index=mid;
+            right--;
+        }
+        else if (nums[mid]<=k)
+        {
+            left++;
+        }
+    }
+    return index;
+}
+int main()
+{
+    vector<int> nums={1,2,2,3};
+    int target=2;
+    cout<<func(nums,target);
+    return 0;
+}
